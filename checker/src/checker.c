162feb0f81d3ddd79c52205dc3b2ac89c0b0f841
Clément Hamada <clementhamada@protonmail.com>
2021-03-10 17:53:46 +0100

    checker: Add exec stub

--- checker/src/checker.c
+++ checker/src/checker.c
@@ -6,20 +6,32 @@ void	print_val(t_stack_val val)
 	ft_dprintf(1, "%hhu, ", val);
 }
 
+int		stack_exec(int fd, t_stack *a)
+{
+	t_stack	*b;
+	char	*line;
+
+	b = NULL;
+	while (get_next_line(fd, &line) != 0)
+	{
+		ft_dprintf(STDOUT_FILENO, "op: %s\n", line);
+	}
+	return (0);
+}
+
 int	main(int ac, char const **av)
 {
-	t_stack	*stack;
+	t_stack	*input_stack;
 
 	if (ac > 1)
 	{
-		stack = stack_load(&av[1]);
-		if (!stack)
+		input_stack = stack_load(&av[1]);
+		if (!input_stack)
 		{
 			ft_dprintf(STDERR_FILENO, "Could not load stack!\n");
 			return (1);
 		}
-		stack_iter(stack, &print_val);
-		ft_dprintf(STDOUT_FILENO, "\n");
+		stack_exec(STDIN_FILENO, input_stack);
 	}
 	return (0);
 }
