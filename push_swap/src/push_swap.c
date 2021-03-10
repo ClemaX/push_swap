91204b1cf951f1a74b010e6f5dd4d76006b45206
Clément Hamada <clementhamada@protonmail.com>
2021-03-10 17:45:09 +0100

    push_swap: Add minimal stack loading code

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -1,4 +1,22 @@
-int main(void)
+#include <stack.h>
+
+#include <stdio.h>
+void	print_val(t_stack_val val)
 {
+	dprintf(1, "%hhu, ", val);
+}
+
+int main(int ac, char const **av)
+{
+	t_stack	*input_stack;
+
+	if (ac > 1)
+	{
+		input_stack = stack_load(&av[1]);
+		if (!input_stack)
+			return (1);
+		stack_iter(input_stack, &print_val);
+		dprintf(1, "\n");
+	}
 	return (0);
 }
