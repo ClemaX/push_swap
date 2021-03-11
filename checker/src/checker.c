ffdd5bb59f930a8ea7521af4ca09e7af489e8c2a
Clément Hamada <clementhamada@protonmail.com>
2021-03-11 17:46:52 +0100

    checker: Parse and execute operations

--- checker/src/checker.c
+++ checker/src/checker.c
@@ -1,22 +1,53 @@
 #include <stack.h>
+#include <stack_op.h>
+
 #include <libft.h>
+#include <gnl/get_next_line.h>
 
 void	print_val(t_stack_val val)
 {
-	ft_dprintf(1, "%hhu, ", val);
+	ft_dprintf(1, "%hhu ", val);
 }
 
-int		stack_exec(int fd, t_stack *a)
+void	print_stack(t_stack *stack, char const *label)
 {
-	t_stack	*b;
-	char	*line;
+	ft_dprintf(STDOUT_FILENO, "stack %s:\n", label);
+	if (stack)
+	{
+		stack_iter(stack, &print_val);
+		ft_dprintf(STDOUT_FILENO, "\n");
+	}
+	else
+		ft_dprintf(STDOUT_FILENO, "empty\n");
+}
+
+int		checker_exec(int fd, t_stack *a)
+{
+	t_stack		*b;
+	t_stack_op	op;
+	char		*line;
+	int			op_err;
+	int			gnl_err;
 
 	b = NULL;
-	while (get_next_line(fd, &line) != 0)
+	op_err = 0;
+	while (!op_err && (gnl_err = get_next_line(fd, &line) ) == GNL_CONTINUE)
 	{
-		ft_dprintf(STDOUT_FILENO, "op: %s\n", line);
+		if ((op = stack_op(line)))
+		{
+			ft_dprintf(STDOUT_FILENO, "executing %s...\n", line);
+			op(&a, &b);
+			print_stack(a, "a");
+			print_stack(b, "b");
+		}
+		else
+		{
+			op_err = 1;
+			ft_dprintf(STDERR_FILENO, "unknown operation: %s!\n", line);
+		}
+		free(line);
 	}
-	return (0);
+	return (gnl_err == GNL_ERROR ? gnl_err : op_err);
 }
 
 int	main(int ac, char const **av)
@@ -31,7 +62,7 @@ int	main(int ac, char const **av)
 			ft_dprintf(STDERR_FILENO, "Could not load stack!\n");
 			return (1);
 		}
-		stack_exec(STDIN_FILENO, input_stack);
+		checker_exec(STDIN_FILENO, input_stack);
 	}
 	return (0);
 }
